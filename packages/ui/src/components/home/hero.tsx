71c388831b3d0e563e091284e6898d669ba67862
Matt FitzGerald-Chamberlain <matt.fitzgerald-chamberlain@codeandtheory.com>
2026-03-05 17:55:16 -0600

    Add missing gap

--- packages/ui/src/components/home/hero.tsx
+++ packages/ui/src/components/home/hero.tsx
@@ -7,7 +7,7 @@ interface HeroProps {
 
 export function Hero({ title, description, image, cta }: HeroProps) {
   return (
-    <div className="flex flex-col items-center md:flex-row">
+    <div className="flex flex-col items-center md:flex-row md:gap-8">
       <div className="flex flex-col gap-4 md:w-2/3">
         <h1 className="font-bold text-6xl">{title}</h1>
         <p className="text-gray-500 text-lg">{description}</p>
