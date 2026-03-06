5e83b2d7d98fdcd45b849072fe5d2c3e13618c97
Matt FitzGerald-Chamberlain <matt.fitzgerald-chamberlain@codeandtheory.com>
2026-03-06 15:44:13 -0600

    Add cache to API

--- apps/web/src/app/api/search/route.ts
+++ apps/web/src/app/api/search/route.ts
@@ -1,7 +1,12 @@
 import { getProducts } from "@repo/api/products";
+import { cacheLife } from "next/cache";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
+  "use cache";
+
+  cacheLife("default");
+
   const { searchParams } = new URL(request.url);
   const query = searchParams.get("query");
 
