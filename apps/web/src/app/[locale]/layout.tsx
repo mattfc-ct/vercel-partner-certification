5dfc49dbcfc59588cd8f8f043cbabf47be86a191
Matt FitzGerald-Chamberlain <matt.fitzgerald-chamberlain@codeandtheory.com>
2026-03-10 16:04:23 -0500

    Set metadataBase

--- apps/web/src/app/[locale]/layout.tsx
+++ apps/web/src/app/[locale]/layout.tsx
@@ -3,14 +3,20 @@ import { Footer } from "@repo/ui/components/layout/footer";
 import { Header } from "@repo/ui/components/layout/header";
 import { Toaster } from "@repo/ui/components/sonner";
 import { routing } from "@repo/ui/i18n/routing";
+import type { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { hasLocale, NextIntlClientProvider } from "next-intl";
 import { getTranslations, setRequestLocale } from "next-intl/server";
 
-export async function generateMetadata() {
+export async function generateMetadata(): Promise<Metadata> {
   const t = await getTranslations("Site");
 
   return {
+    metadataBase: new URL(
+      process.env.VERCEL_URL
+        ? `https://${process.env.VERCEL_URL}`
+        : "http://localhost:3000/"
+    ),
     title: {
       template: t("metaTitle"),
       default: t("defaultMetaTitle"),
